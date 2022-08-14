c76f9d46af0055db8600a6e8ec34a9ad850e1899
SandyEwais <89739431+SandyEwais@users.noreply.github.com>
2022-08-15 00:43:15 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,7 +6,7 @@ import { PaintingComponent } from './painting/painting.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: '/', redirectTo: 'login', pathMatch: 'full'},
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent },
   { path: 'painting/:type/:id', component: PaintingComponent },
